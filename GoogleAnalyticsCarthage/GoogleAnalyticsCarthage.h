1c42bf42317d133137e1669f0d0951724a78495d
Eugeniy Popov <hey.makemescream@gmail.com>
2017-12-16 18:20:56 +0300

    add pod

---
+++ GoogleAnalyticsCarthage/GoogleAnalyticsCarthage.h
@@ -0,0 +1,19 @@
+//
+//  GoogleAnalyticsCarthage.h
+//  GoogleAnalyticsCarthage
+//
+//  Created by Eugeniy Popov on 16.12.2017.
+//  Copyright © 2017 Eugeniy Popov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GoogleAnalyticsCarthage.
+FOUNDATION_EXPORT double GoogleAnalyticsCarthageVersionNumber;
+
+//! Project version string for GoogleAnalyticsCarthage.
+FOUNDATION_EXPORT const unsigned char GoogleAnalyticsCarthageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GoogleAnalyticsCarthage/PublicHeader.h>
+
+
---
+++ Pods/Target Support Files/Pods-GoogleAnalyticsCarthage/Pods-GoogleAnalyticsCarthage-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GoogleAnalyticsCarthage : NSObject
+@end
+@implementation PodsDummy_Pods_GoogleAnalyticsCarthage
+@end
